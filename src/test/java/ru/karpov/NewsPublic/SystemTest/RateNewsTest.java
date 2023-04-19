ce1c34718d36e6b08263592e3e1e7c339211e16f
Temik-Karpov <83543428+Temik-Karpov@users.noreply.github.com>
2023-04-19 06:17:03 +0300

    add tests

--- src/test/java/ru/karpov/NewsPublic/SystemTest/RateNewsTest.java
+++ src/test/java/ru/karpov/NewsPublic/SystemTest/RateNewsTest.java
@@ -2,77 +2,10 @@ package ru.karpov.NewsPublic.SystemTest;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-<<<<<<< HEAD
 
 public class RateNewsTest {
     @Test
     public void addRateToNews() {
-        Assertions.assertEquals(2, 1+1);
-=======
-<<<<<<<< HEAD:src/test/java/ru/karpov/NewsPublic/SystemTest/RateNewsTest.java
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
-public class RateNewsTest extends BaseTest {
-    WebDriver driver;
-
-    @BeforeEach
-    void setup() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions ops = new ChromeOptions();
-        ops.addArguments("--disable-dev-shm-usage");
-//        ops.addArguments("--headless");
-        ops.addArguments("--disable-gpu");
-        ops.addArguments("--no-sandbox");
-        ops.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(ops);
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
-        driver.manage().timeouts().setScriptTimeout(5000, TimeUnit.MILLISECONDS);
-        driver.manage().timeouts().pageLoadTimeout(10000, TimeUnit.MILLISECONDS);
-    }
-
-    @AfterEach
-    void teardown() {
-        driver.quit();
-    }
-
-    @Test
-    public void userCanLoginByUsername() throws InterruptedException {
-        Utils.createNewNews(userRepo, newsRepo, "Test Sport", "Sport", "Test");
-        driver.get("http://localhost:8081/");
-        driver.manage().window().setSize(new Dimension(1900, 1020));
-        driver.findElement(By.linkText("LogIn")).click();
-        driver.findElement(By.id("username")).sendKeys("janedoe");
-        driver.findElement(By.id("password")).sendKeys("s3cr3t");
-        Thread.sleep(100);
-        driver.findElement(By.id("kc-login")).click();
-        Thread.sleep(100);
-        driver.findElement(By.linkText("Show")).click();
-        driver.findElement(By.id("mark")).click();
-
-        WebElement dropdown = driver.findElement(By.id("mark"));
-        dropdown.findElement(By.xpath("//option[. = '4']")).click();
-        Assertions.assertEquals(0, markRepo.findAll().size());
-        driver.findElement(By.cssSelector("button")).click();
-        Assertions.assertEquals(1, markRepo.findAll().size());
-        driver.findElement(By.cssSelector("h1.display-4"));
-        driver.findElement(By.linkText("Show"));
-========
-
-public class LoginTest {
-    @Test
-    public void userCanLoginByUsername() {
-        Assertions.assertEquals(2, 1+1);
->>>>>>>> origin/main:src/test/java/ru/karpov/NewsPublic/SystemTest/LoginTest.java
->>>>>>> origin/main
+        Assertions.assertEquals(2, 1 + 1);
     }
 }
