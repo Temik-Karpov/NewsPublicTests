857db1ce0b21e635234200254218b7c4e82e2767
Temik-Karpov <83543428+Temik-Karpov@users.noreply.github.com>
2023-04-19 07:41:09 +0300

    add tests

---
+++ src/test/java/ru/karpov/NewsPublic/SystemTest/ChangeMarkTest.java
@@ -0,0 +1,11 @@
+package ru.karpov.NewsPublic.SystemTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ChangeMarkTest {
+    @Test
+    public void ChangeMarkTest() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+}
---
+++ src/test/java/ru/karpov/NewsPublic/SystemTest/addEmptyPostTest.java
@@ -0,0 +1,11 @@
+package ru.karpov.NewsPublic.SystemTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class addEmptyPostTest {
+    @Test
+    public void addEmptyPostTest() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+}
