5534f8e2a83b19340fe653183df3ab9c0342eeb1
Temik-Karpov <83543428+Temik-Karpov@users.noreply.github.com>
2023-04-19 06:34:29 +0300

    add tests

---
+++ src/test/java/ru/karpov/NewsPublic/SystemTest/InvalidRegistrationAuthenticationTest.java
@@ -0,0 +1,11 @@
+package ru.karpov.NewsPublic.SystemTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InvalidRegistrationAuthenticationTest {
+    @Test
+    public void userPutInvalidInfoInRegistration() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+}
