0611482ae83dce5c83d50104c99bfe68e02beffd
Temik-Karpov <83543428+Temik-Karpov@users.noreply.github.com>
2023-04-19 07:17:04 +0300

    add tests

---
+++ src/test/java/ru/karpov/NewsPublic/SystemTest/LogoutTest.java
@@ -0,0 +1,11 @@
+package ru.karpov.NewsPublic.SystemTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LogoutTest {
+    @Test
+    public void logoutTest() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+}
---
+++ src/test/java/ru/karpov/NewsPublic/SystemTest/SubscribeAnotherUserTest.java
@@ -0,0 +1,11 @@
+package ru.karpov.NewsPublic.SystemTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SubscribeAnotherUserTest {
+    @Test
+    public void userCanSubscribeAnotherUser() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+}
