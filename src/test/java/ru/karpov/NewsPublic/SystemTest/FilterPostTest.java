b3eadb6816f8308359fd9e5207273520bbfc5148
Temik-Karpov <83543428+Temik-Karpov@users.noreply.github.com>
2023-04-19 06:05:34 +0300

    add tests

---
+++ src/test/java/ru/karpov/NewsPublic/SystemTest/FilterPostTest.java
@@ -0,0 +1,11 @@
+package ru.karpov.NewsPublic.SystemTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FilterPostTest {
+    @Test
+    public void showNewsByCategory() {
+        Assertions.assertEquals(2, 1+1);
+    }
+}
